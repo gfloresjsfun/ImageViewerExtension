447774e55ff876175d56ff8504e936b0329ca13e
gfloresjsfun <gfloresjswork@gmail.com>
2022-10-13 19:46:22 +0500

    video url check added

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.get("/fileUpload", middleware.authenticated, async (req, res) => {
     .then((resp) => {
       console.log("resp = ", resp);
       //   res.sendFile(resp);
-      let imagePath= resp.replace(__dirname,'');
-      console.log("imagePath = ",imagePath)
+      let imagePath = resp.replace(__dirname, "");
+      console.log("imagePath = ", imagePath);
 
       res.json(imagePath);
     })
@@ -41,46 +41,48 @@ app.get("/imageViewer", async (req, res) => {
   );
 });
 
-app.get("/fileLink", async (req,res)=>{
-    console.log("req.query.url = ",req.query.url);
+app.get("/fileLink", async (req, res) => {
+  console.log("req.query.url = ", req.query.url);
 
-    res.sendFile(__dirname+req.query.url);
-    // fs.readFile(
-    //   __dirname + "/ImageViewer/imageLoad.html",
-    //   "utf8",
-    //   (err, text) => {
-    //     res.send(text);
-    //   }
-    // );
+  res.sendFile(__dirname + req.query.url);
+  // fs.readFile(
+  //   __dirname + "/ImageViewer/imageLoad.html",
+  //   "utf8",
+  //   (err, text) => {
+  //     res.send(text);
+  //   }
+  // );
+});
 
-})
-
-app.get("/imageLink", middleware.authenticated, async (req,res)=>{
-
+app.get("/imageLink", middleware.authenticated, async (req, res) => {
   fileDownload
-  .downloadFile(req.query.url, "images")
-  .then((resp) => {
-    console.log("resp = ", resp);
-    //   res.sendFile(resp);
-    let imagePath= resp.replace(__dirname,'');
-    console.log("imagePath = ",imagePath)
+    .downloadFile(req.query.url, "images")
+    .then((resp) => {
+      console.log("resp = ", resp);
+      //   res.sendFile(resp);
+      let imagePath = resp.replace(__dirname, "");
+      console.log("imagePath = ", imagePath);
 
-    // res.json(imagePath);
-    let newerPath= `http://localhost:3000/fileLink?url=${imagePath}`
-    res.send(`
-    <div>
-      <img src="${newerPath} "style="
-      width: 100%;
-      height: auto;
-    " />
-    </div>
-  `)
-  })
-  .catch((err) => {
-    res.status(400).json({ error: err });
-  });
-
-})
+      // res.json(imagePath);
+      let newerPath = `http://localhost:3000/fileLink?url=${imagePath}`;
+      const validImageTypes = ["gif", "jpeg", "png" , "jpg"];
+      if (!newerPath.includes(validImageTypes)) {
+        res.redirect(newerPath);
+      } else {
+        res.send(`
+      <div>
+        <img src="${newerPath} "style="
+        width: 100%;
+        height: auto;
+      " />
+      </div>
+    `);
+      }
+    })
+    .catch((err) => {
+      res.status(400).json({ error: err });
+    });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
